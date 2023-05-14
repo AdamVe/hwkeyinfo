be44fa5f16393d71cf0e64a8fe211123052cea7e
Adam Velebil <pardalek@gmail.com>
2023-05-14 13:55:20 +0200

    styling SecurityKeyCard

--- app/src/main/java/com/adamve/hwkeyinfo/ui/security_key/SecurityKeyCard.kt
+++ app/src/main/java/com/adamve/hwkeyinfo/ui/security_key/SecurityKeyCard.kt
@@ -1,36 +1,28 @@
 package com.adamve.hwkeyinfo.ui.security_key
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.adamve.hwkeyinfo.data.SecurityKey
 import com.adamve.hwkeyinfo.data.SecurityKeyWithServices
 import com.adamve.hwkeyinfo.data.Service
@@ -38,34 +30,64 @@ import com.adamve.hwkeyinfo.ui.theme.HwKeyInfoTheme
 
 @Composable
 fun ServiceTag(
-    service: Service
+    service: Service,
+    modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .padding(bottom = 2.dp)
-            .clip(RoundedCornerShape(3.dp))
-            .padding(horizontal = 1.dp)
-            .border(
-                border = BorderStroke(0.1.dp, color = Color.Black),
-                shape = RoundedCornerShape(corner = CornerSize(3.dp))
-            )
-            .padding(2.dp)
-            .widthIn(max = 100.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .padding(horizontal = 2.dp)
+            .background(MaterialTheme.colorScheme.surfaceColorAtElevation(0.dp))
+            .padding(4.dp)
     ) {
         Text(
             service.serviceName,
-            fontSize = 8.sp,
-            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.bodySmall.copy(
+                fontWeight = FontWeight.SemiBold
+            ),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis
         )
-        Text(service.serviceUser, fontSize = 8.sp, maxLines = 1, overflow = TextOverflow.Ellipsis)
+        Text(
+            service.serviceUser,
+            style = MaterialTheme.typography.bodySmall.copy(
+                fontWeight = FontWeight.ExtraLight
+            ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
 @Composable
 fun KeyIdentifier(identifier: String, modifier: Modifier = Modifier) {
-    Text(identifier, modifier = modifier, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+    Text(
+        identifier,
+        modifier = modifier,
+        fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+        fontWeight = MaterialTheme.typography.headlineLarge.fontWeight
+    )
+}
+
+@Composable
+fun Headline(securityKey: SecurityKey, modifier: Modifier = Modifier) {
+    if (securityKey.name.isNotBlank() && securityKey.type.isNotBlank()) {
+        Column {
+            KeyIdentifier(securityKey.name, modifier)
+            Text(
+                securityKey.type,
+                fontSize = MaterialTheme.typography.labelLarge.fontSize,
+                fontWeight = FontWeight.Light,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                maxLines = 1,
+            )
+        }
+    } else if (securityKey.name.isNotBlank()) {
+        KeyIdentifier(securityKey.name)
+    } else {
+        KeyIdentifier(securityKey.type)
+    }
 }
 
 @OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
@@ -82,79 +104,83 @@ fun SecurityKeyCard(
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .fillMaxWidth()
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Row(
-                verticalAlignment = Alignment.Bottom
-            ) {
-                if (securityKey.name.isNotBlank() && securityKey.type.isNotBlank()) {
-                    KeyIdentifier(securityKey.name, Modifier.alignByBaseline())
-                    //Text(" / ", modifier = Modifier.alignByBaseline(), fontSize = 14.sp)
-                    Text(
-                        " " + securityKey.type,
-                        modifier = Modifier.alignByBaseline(),
-                        fontSize = 12.sp
-                    )
-                } else if (securityKey.name.isNotBlank()) {
-                    KeyIdentifier(securityKey.name)
-                } else {
-                    KeyIdentifier(securityKey.type)
-                }
-            }
-
+        Column(
+            modifier = Modifier.padding(
+                horizontal = 16.dp,
+                vertical = 8.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Headline(securityKey)
             if (securityKey.description.isNotBlank()) {
                 Text(
                     securityKey.description,
-                    fontSize = 10.sp,
-                    fontStyle = FontStyle.Italic
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontStyle = FontStyle.Italic,
+                        lineHeight = MaterialTheme.typography.bodyMedium.lineHeight
+                    )
                 )
             }
 
-            if (securityKeyWithServices.services.isNotEmpty()) {
-                Text(
-                    "Services: ${securityKeyWithServices.services.size}",
-                    fontSize = 12.sp,
-                    modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
-                )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp))
+                    .padding(8.dp)
+            ) {
 
-                FlowRow(
-                    verticalAlignment = Alignment.Top,
-                    horizontalArrangement = Arrangement.spacedBy(5.dp)
-                ) {
-                    securityKeyWithServices.services.forEach {
-                        ServiceTag(it)
+
+                if (securityKeyWithServices.services.isNotEmpty()) {
+                    Text(
+                        "Services: ${securityKeyWithServices.services.size}",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+                    )
+
+                    FlowRow(
+                        verticalAlignment = Alignment.Top,
+                        horizontalArrangement = Arrangement.spacedBy(5.dp)
+                    ) {
+                        securityKeyWithServices.services.sortedBy {
+                            val nameLen = it.serviceName.length
+                            val userLen = it.serviceUser.length
+                            userLen
+                        }.forEach {
+                            ServiceTag(
+                                it,
+                                modifier = Modifier.fillMaxWidth(fraction = 0.49f)
+                            )
+                        }
                     }
+                } else {
+                    Text(
+                        "No services",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+                    )
                 }
-            } else {
-                Text(
-                    "No services",
-                    fontSize = 12.sp,
-                    fontStyle = FontStyle.Italic,
-                    modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
-                )
             }
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview
+@Preview(uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun SecurityKeyCardPreview() {
-    val key = SecurityKey(
-        0,
-        "Key name",
-        "Key nickname",
-        "Key type",
-        "ref",
-        "Key description"
-    )
-
     HwKeyInfoTheme {
-        SecurityKeyCard(SecurityKeyWithServices(securityKey = key, services = listOf()))
+        SecurityKeyCard(
+            SecurityKeyWithServices(
+                securityKey = previewSecurityKey,
+                services = listOf()
+            )
+        )
     }
 
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview
 @Composable
 fun SecurityKeyWithoutNameCardPreview() {
     val key = SecurityKey(0, "", "Key nickname", "Key type", "ref", "Key description")
@@ -165,7 +191,7 @@ fun SecurityKeyWithoutNameCardPreview() {
 
 }
 
-@Preview(showBackground = true, widthDp = 320, showSystemUi = true)
+@Preview
 @Composable
 fun SecurityKeyWithoutDescriptionPreview() {
     val key = SecurityKey(0, "Key name", "Key nickname", "", "ref", "")
@@ -201,3 +227,12 @@ fun SecurityKeyWithoutDescriptionPreview() {
     }
 
 }
+
+val previewSecurityKey = SecurityKey(
+    0,
+    "Private backup Key",
+    "HW Key Nick",
+    "HW Key Type",
+    "ref",
+    "This is my main backup key and it is stored in bank X ref 123."
+)
